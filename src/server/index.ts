45edb798ebd9f88a0abc282172bf39bdb36dc21d
Octavio Cuenca <octavio@Octavios-MacBook-Pro.local>
2023-11-22 00:38:13 -0500

    first commit

---
+++ src/server/index.ts
@@ -0,0 +1,14 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+const port = 3001;
+
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello, TypeScript with Node.js!");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
